c5bec47dcd536cd9be63046b85a27bba22437284
Nikolay Rogozin <norogozin@gmail.com>
2022-11-18 05:42:36 +0400

    Send console input from client

--- client.js
+++ client.js
@@ -1,13 +1,25 @@
 import net from 'net';
-const client = new net.Socket();
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
 const port = 8070;
 const host = '127.0.0.1';
+//const client = new net.Socket({address:`${host}:${port}`});
+const client = new net.Socket();
+
+const readSendLoop = async () => {
+    const rl = await readline.createInterface({ input, output });
+    rl.on('line', (input) => {
+        client.write( `${input}`);
+      });
+};
 
 client.connect(port, host, function() {
     console.log('Connected');
     client.write("Hello From Client " + client.address().address);
-    //client.close();
+    readSendLoop();
 });
+//repl
 
 client.on('data', function(data) {
     console.log('Server Says : ' + data);
